a5d0d24ab2806d118572c3d4787364e12786772c
onurk <onurk@DESKTOP-7FVNJAI>
2022-12-19 22:27:05 +0300

    Add project files.

---
+++ 17.12.2022_Ödev_ Burçlar/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace _17._12._2022_Ödev__Burçlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btn_Hesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(204, 41);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(341, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // btn_Hesapla
+            // 
+            this.btn_Hesapla.Location = new System.Drawing.Point(285, 93);
+            this.btn_Hesapla.Name = "btn_Hesapla";
+            this.btn_Hesapla.Size = new System.Drawing.Size(161, 43);
+            this.btn_Hesapla.TabIndex = 1;
+            this.btn_Hesapla.Text = "Hesapla";
+            this.btn_Hesapla.UseVisualStyleBackColor = true;
+            this.btn_Hesapla.Click += new System.EventHandler(this.btn_Hesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Yaş";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Yaşadığı Gün";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Burç";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(346, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(346, 200);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(346, 236);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Hesapla);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btn_Hesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ 17.12.2022_Ödev_ Burçlar/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17._12._2022_Ödev__Burçlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Hesapla_Click(object sender, EventArgs e)
+        {
+            int a = (DateTime.Now.Year - dateTimePicker1.Value.Year);
+            TimeSpan b = DateTime.Now - dateTimePicker1.Value;
+            textBox1.Text = $"{a}";
+            textBox2.Text = b.Days.ToString();
+            
+            int gun = dateTimePicker1.Value.DayOfYear;
+
+            if (gun < 51 && gun >= 20)
+            {
+                textBox3.Text = "kova";
+            }
+            else if (gun < 80 && gun >= 51)
+            {
+                textBox3.Text = "Balık";
+            }
+            else if (gun < 111 && gun >= 80)
+            {
+                textBox3.Text = "Koç";
+            }
+            else if (gun < 141 && gun >= 111)
+            {
+                textBox3.Text = "Boğa";
+            }
+            else if (gun < 173 && gun >= 141)
+            {
+                textBox3.Text = "İkizler";
+            }
+            else if (gun < 204 && gun >= 173)
+            {
+                textBox3.Text = "Yengeç";
+            }
+            else if (gun < 136 && gun >= 204)
+            {
+                textBox3.Text = "Aslan";
+            }
+            else if (gun < 267 && gun >= 136)
+            {
+                textBox3.Text = "Başak";
+            }
+            else if (gun < 297 && gun >= 267)
+            {
+                textBox3.Text = "Terazi";
+            }
+            else if (gun < 327 && gun >= 297)
+            {
+                textBox3.Text = "Akrep";
+            }
+            else if (gun < 356 && gun >= 327)
+            {
+                textBox3.Text = "Yay";
+            }
+            else if (gun < 20 || (gun > 356 && gun < 366))
+            {
+                textBox3.Text = "Oğlak";
+            }
+
+
+        }
+    }
+}
